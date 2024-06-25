926b26f8704938f57931bd6a8516fe69c57bdea7
anmoldubey <mayankdubey2673@gmail.com>
2024-06-25 15:42:04 +0530

    Refactor: Removed deprecated MongoDB options from connectDB.js

--- dbConnect/Config.js
+++ dbConnect/Config.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("DB Connection Successful");
     } catch (err) {
-        console.log("Error while connecting to DB:", err);
+        console.error("Error while connecting to DB:", err);
         process.exit(1); // Exit process with failure
     }
 };
